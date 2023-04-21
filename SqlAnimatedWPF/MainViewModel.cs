74790a5beda5c1868aaaa04eb648ad720fe01e19
122tp12 <63251202+122tp12@users.noreply.github.com>
2023-04-21 21:26:01 +0300

    Add project files.

---
+++ SqlAnimatedWPF/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlAnimatedWPF
+{
+    public class MainViewModel
+    {
+        private SqLiteModel model;
+        
+        public MainViewModel()
+        {
+            model = new SqLiteModel();
+        }
+    }
+}
---
+++ SqlAnimatedWPF/SqLiteModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SQLite;
+
+namespace SqlAnimatedWPF
+{
+    public class SqLiteModel
+    {
+        public SqLiteModel()
+        {
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+
+            string connectionString = "Data Source=MyDatabase.sqlite;Version=3;";
+            SQLiteConnection m_dbConnection = new SQLiteConnection(connectionString);
+            m_dbConnection.Open();
+
+            string sql = "CREATE TABLE \"comment\" (\"id\" INTEGER,\"applicationName\" TEXT,\"userName\" TEXT,\"comment\" TEXT, PRIMARY KEY(\"id\" AUTOINCREMENT));";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            
+
+            m_dbConnection.Close();
+        }
+    }
+}
